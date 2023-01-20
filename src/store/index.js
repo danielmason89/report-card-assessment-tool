35b373104245d4c9503293a36040f07d27e2805c
Daniel Mason <danmasson0@gmail.com>
2023-01-20 13:17:03 -0500

    updated development app run script, and start implement vuex with state management

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,82 @@
-import { createStore } from 'vuex'
+import { createStore } from "vuex";
+import {
+  ADD_TEACHER_CLASSLIST,
+  DELETE_TEACHER_CLASSLIST,
+  GET_TEACHER_CLASSLIST,
+  UPDATE_TEACHER_CLASSLIST,
+} from "./mutation-types";
 
 export default createStore({
+  strict: process.env.NODE_ENV === "production",
   state: {
+    teacherClasslist: [],
   },
   getters: {
+    getTeacherClasslistById: (state) => (id) => {
+      return state.teacherClasslist.find(
+        (teacherClasslist) => teacherClasslist.id === id
+      );
+    },
   },
   mutations: {
+    [GET_TEACHER_CLASSLIST](state, teacherClasslist) {
+      state.teacherClasslist = teacherClasslist;
+    },
+    [ADD_TEACHER_CLASSLIST](state, teacherClasslist) {
+      state.teacherClasslist.push(teacherClasslist);
+    },
+    [DELETE_TEACHER_CLASSLIST](state, teacherClasslist) {
+      state.teacherClasslists = [
+        ...state.teacherClasslists.filter(
+          (classList) => classList.id !== teacherClasslist.id
+        ),
+      ];
+    },
+    [UPDATE_TEACHER_CLASSLIST](state, teacherClasslist) {
+      const index = state.teacherClasslist.findIndex(
+        (teacherClasslist) => teacherClasslist.id === id
+      );
+      state.teacherClasslist.splice(index, 1, teacherClasslist);
+      state.teacherClasslist = [...state.teacherClasslist];
+    },
   },
   actions: {
+    async getTeacherClasslistAction({ commit }) {
+      const teacherClasslist = await fetch(
+        "http://localhost:3000/teacherClasslist"
+      );
+      commit([GET_TEACHER_CLASSLIST], teacherClasslist);
+    },
+    async addTeacherClasslistAction({ commit }, teacherClasslist) {
+      const addStudent = await fetch("http://localhost:3000/teacherClasslist", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(teacherClasslist),
+      });
+      commit([ADD_TEACHER_CLASSLIST], addStudent);
+    },
+    async deleteTeacherClasslistAction({ commit }, teacherClasslist) {
+      const deleteStudentId = await fetch(
+        "http://localhost:3000/teacherClasslist",
+        {
+          method: "DELETE",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(teacherClasslist),
+        }
+      );
+      commit([DELETE_TEACHER_CLASSLIST], deleteStudentId);
+    },
+    async updateTeacherClasslistAction({ commit }, teacherClasslist) {
+      const updateStudent = await fetch(
+        "http://localhost:3000/teacherClasslist",
+        {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(teacherClasslist),
+        }
+      );
+      commit([UPDATE_TEACHER_CLASSLIST], updateStudent);
+    },
   },
-  modules: {
-  }
-})
+  modules: {},
+});
---
+++ src/store/mutation-types.js
@@ -0,0 +1,4 @@
+export const GET_TEACHER_CLASSLIST = "getTeacherClasslist";
+export const ADD_TEACHER_CLASSLIST = "addTeacherClasslist";
+export const UPDATE_TEACHER_CLASSLIST = "updateTeacherClasslist";
+export const DELETE_TEACHER_CLASSLIST = "deleteTeacherClasslist";
